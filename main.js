e9292ca236e249f4c8405649b6c9b5419af79736
Isming <email@example.com>
2026-04-07 10:42:29 +0500

    jjjj

---
+++ main.js
@@ -0,0 +1,39 @@
+// let  a = prompt("a sonini liriting");
+// let  n = prompt("n sonini liriting");
+// function power(a, n){
+//     alert(Math.pow(a,n));
+
+// }
+// console.log(power(a,n));
+
+
+
+// function mean(a, b){
+// let arifmetigi = (a + b) / 2;
+// let geometirigi = Math.sqrt(a * b);
+
+// return[arifmetigi, geometirigi]
+
+// }
+// console.log(mean(12 , 48));
+
+
+
+let son = prompt("son kiriting");
+function  sign(n){
+    if (n < 0) {
+        alert(son --);
+
+        
+    }
+     else if (son >0){
+        alert(son ++);
+          
+        
+    }
+    else(son === 0 ){
+        alert(0);
+
+    }
+}
+console.log(sign(son));
